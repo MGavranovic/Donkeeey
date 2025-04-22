a6842df03a390b5896fd49748ef092d7a4925c2b
Milos Gavranovic <mgavranovic7@gmail.com>
2025-04-22 13:12:22 +0200

    remove constants from gitignore

---
+++ constants.lua
@@ -0,0 +1,11 @@
+-- NOTE: Card base values for the card object
+-- these are going to be the cards for the base game of collection
+local constants = {
+    Card_height = 360,
+    Card_width = 240
+}
+
+return constants
+
+
+-- TODO: we can have nested tables to group constants
