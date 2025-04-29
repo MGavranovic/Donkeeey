59102574a8f000ba47623d39393566451b5cd52e
Milos Gavranovic <mgavranovic7@gmail.com>
2025-04-29 10:18:18 +0200

    Add random colors to facedown cards / Change 2♣ to red

--- card/card.lua
+++ card/card.lua
@@ -9,16 +9,27 @@ function Card:new(name)
     instance.name = name
     instance.height = c.Card_height
     instance.width = c.Card_width
+
+    instance.back_color = {
+        r = love.math.random(),
+        g = love.math.random(),
+        b = love.math.random()
+    }
+
     return instance
 end
 
 function Card:draw(faceup)
-    if faceup or self.name == "2 Club" then
+    if faceup then
         lg.setColor(1, 1, 1)
         lg.rectangle("line", self.x, self.y, self.width, self.height)
         lg.print(self.name, self.x + 5, self.y + 5)
+    elseif self.name == "2 Club" then
+        lg.setColor(1, 0, 0)
+        lg.rectangle("line", self.x, self.y, self.width, self.height)
+        lg.print(self.name, self.x + 5, self.y + 5)
     else
-        lg.setColor(0.2, 0.2, 0.2) -- back of the card
+        lg.setColor(self.back_color.r, self.back_color.g, self.back_color.b) -- back of the card
         lg.rectangle("fill", self.x, self.y, self.width, self.height)
 
         lg.setColor(1, 1, 1)
